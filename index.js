d2eae29a63f784841226cbd9c8adc647c42896d8
Facundo Merighi <merighifacundo@gmail.com>
2021-06-12 11:51:24 -0300

    Trying a older version of github

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ try {
   
   core.setOutput("link", "http://google.com");
   // Get the JSON webhook payload for the event that triggered the workflow
-  
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
