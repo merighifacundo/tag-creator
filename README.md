41571893c9b1d219b4ea2d66dd28389f83a21743
Facundo Merighi <merighifacundo@gmail.com>
2021-06-12 11:15:25 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # tag-creator
+
+This is a GitHub Action to version a branch with a number and create the tag and push it.
+
+
+## Example usage
+
+uses: actions/tag-creator@v1.0.0
+with:
+  new-version: '1.0.2'
---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  
+  const newVersion = core.getInput('new-version');
+  console.log(`New version to get updated ${newVersion}!`);
+  
+  core.setOutput("link", "http://google.com");
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
